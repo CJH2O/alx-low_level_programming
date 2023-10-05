41b4b58933fa3a78c04ec97e3000bdc5d9a8c9ff
CJH2O <cjh2osocial@gmail.com>
2023-10-05 13:10:34 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -is the point of entry
+ *
+ * Return:0 always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
